a94405b03d97fc51bd82b0779c767c8bb0b01ab4
Daniel <Minecraftgamerpc64@gmail.com>
2018-04-21 20:25:48 -0400

    test

---
+++ snow man boss/Assets/code/playercontroller.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+    public float movex;
+    public float speed;
+    public float jumpForce;
+
+    public AudioSource jumping;
+    
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movex = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(movex * speed, rb2d.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+
+            
+        }
+    }
+}
